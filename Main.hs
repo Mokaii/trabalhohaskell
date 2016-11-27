3161e427ec1af8b43ff07f161068379445c90794
Rafael Viegas <rafatroopa@gmail.com>
2016-11-27 21:09:12 +0000

    Main.hs alterado

--- Main.hs
+++ Main.hs
@@ -1,6 +1,20 @@
-import Application () -- for YesodDispatch instance
+{-# LANGUAGE OverloadedStrings, QuasiQuotes,
+             TemplateHaskell #-}
+ 
+module Main where
+import Application
+import Yesod
+import Yesod.Static
 import Foundation
-import Yesod.Core
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Applicative
+import Data.Text
+import Database.Persist.Postgresql
 
-main :: IO ()
-main = warp 3000 App
+connStr :: ConnectionString
+connStr = "dbname=dfe27dgrck3kt0 host=ec2-54-235-65-221.compute-1.amazonaws.com user=ibneirltsjezhx password=hYNq-AgbiH6E6BW9fJgnT1BjcB port=5432"
+
+main::IO()
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
+       runSqlPersistMPool (runMigration migrateAll) pool 
+       warp 8080 (Sitio pool)
