799661b874d622bf604f87b8e1037ace4d235efe
Rafael Viegas <rafatroopa@gmail.com>
2016-11-27 21:17:54 +0000

    crud funcionario 2

--- Handlers/Funcionario.hs
+++ Handlers/Funcionario.hs
@@ -29,3 +29,41 @@ getCadastroFuncionarioR = do
                  ^{widget}
                  <input type="submit" value="Cadastrar">
            |]
+           
+getFuncionarioR :: FuncionarioId -> Handler Html
+getFuncionarioR pid = do
+             funcionario <- runDB $ get404 pid 
+             defaultLayout [whamlet| 
+                <head>
+                  <title>Bootstrap Example</title>
+                  <meta charset="utf-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                <body>
+                    <div class="container">
+                        <div class="jumbotron">
+                            <h3 style="text-align:center; align:center;"><center>Voce esta consultando o funcionario: <b>#{funcionarioNm_funcionario funcionario}
+                    <div class="container" style="background-color:#ad4f4 text-align:center;" >
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th>ID
+                            <th>Nome
+                            <th>Cargo
+                            <th>CPF
+                            <th>Email
+                            <th>Data Nascimento
+                            <th>Deseja Excluir?
+                            <th>Deseja Voltar?
+                          <tr>
+                            <td><a href=@{FuncionarioR pid}>#{fromSqlKey pid}
+                            <td>#{funcionarioNm_funcionario funcionario}
+                            <td>#{funcionarioCargo_funcionario funcionario}
+                            <td>#{funcionarioCpf_funcionario funcionario}
+                            <td>#{funcionarioEmail_funcionario funcionario}
+                            <td>#{funcionarioDt_nascimento funcionario}
+                            <td><form method=post action=@{FuncionarioR pid}><input type="submit" value="Deletar">
+                            <td><a href=@{ListFuncionarioR}><button type="button" class="btn-warning">Voltar
+             |]
