cc14ead449b28435ea152448d1a6fb10cb5f09df
Rafael Viegas <rafatroopa@gmail.com>
2016-11-27 21:20:33 +0000

    crud funcionario 3

--- Handlers/Funcionario.hs
+++ Handlers/Funcionario.hs
@@ -67,3 +67,63 @@ getFuncionarioR pid = do
                             <td><form method=post action=@{FuncionarioR pid}><input type="submit" value="Deletar">
                             <td><a href=@{ListFuncionarioR}><button type="button" class="btn-warning">Voltar
              |]
+
+getListFuncionarioR :: Handler Html
+getListFuncionarioR = do
+            funcionarios <- runDB $ selectList [] [Asc FuncionarioNm_funcionario]
+            defaultLayout $ do
+                [whamlet|
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                        <title>Consulta de funcionarios</title>
+
+                    <body>
+                        <div class="container">
+                            <div class="jumbotron">
+                                <h3 style="text-align:center; align:center;">FUNCIONARIOS CADASTRADOS
+
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID
+                                    <th>Nome
+                                    <th>Cargo
+                                    <th>CPF
+                                    <th>Email
+                                    <th>Data Nascimento
+                                    <th>Deseja Voltar?
+
+                         $forall Entity pid funcionario <- funcionarios
+                             <tbody>
+                                 <tr>
+                                     <td><a href=@{FuncionarioR pid}>#{fromSqlKey pid}
+                                     <td>#{funcionarioNm_funcionario funcionario}
+                                     <td>#{funcionarioCargo_funcionario funcionario}
+                                     <td>#{funcionarioCpf_funcionario funcionario}
+                                     <td>#{funcionarioEmail_funcionario funcionario}
+                                     <td>#{funcionarioDt_nascimento funcionario}
+                                     <td><a href=@{HomeR}><button type="button" class="btn-warning">Voltar
+
+                |]
+
+
+postCadastroFuncionarioR :: Handler Html
+postCadastroFuncionarioR = do
+            ((result, _), _) <- runFormPost formFuncionario
+            case result of
+                FormSuccess funcionario -> do
+                    pid <- runDB $ insert funcionario
+                    defaultLayout [whamlet|
+                        Funcionário cadastrado com sucesso #{fromSqlKey pid}!
+                    |]
+                _ -> redirect HomeR
+
+
+postFuncionarioR :: FuncionarioId -> Handler Html
+postFuncionarioR pid = do
+     runDB $ delete pid
+     redirect ListFuncionarioR
